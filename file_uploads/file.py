b186e1b53075b525ee1b16738078fba66d20111f
ouyangsiying <785339684@qq.com>
2018-07-06 11:27:59 +0800

    图片、文件上传的方法（足协信息修改的例子）

--- file_uploads/file.py
+++ file_uploads/file.py
@@ -1,24 +1,29 @@
 from NetworkRequest import NetworkRequest
 from requests_toolbelt import MultipartEncoder
+import json
 
 n = NetworkRequest()
 n.login("admin001","111111")
 token = n.getNowToken()
-
+#处理上传文件
 m = MultipartEncoder(
     fields = {
         "_token":token,
-        "image":("a.png",open("./file_uploads/a.png","rb"),"image/png")
+        "image":("a.png",open("E:\\python\\study\\file_uploads\\a.png","rb"),"image/png")
     }
 )
 req = n.post2("/api/tempimage",m,m.content_type)
 print(req.content)
-# data = {
-#     "logo":"2018-07-06_10.34.48-a49a34942884d1270da373eb44aea31e.jpg",
-#     "_token":token,
-#     "_method":"put",
-#     "fa_base_id":76
-# }
-#
-# req = n.post("fa/update",data)
-# print(req.text)
+fa_logo = json.loads(req.content)["result"]["tmp_path"][18:]
+
+print(fa_logo)
+#更新足协基本信息
+data = {
+    "logo":fa_logo,
+    "_token":token,
+    "_method":"put",
+    "fa_base_id":76
+}
+
+req = n.post("fa/update",data)
+print(req.text)
