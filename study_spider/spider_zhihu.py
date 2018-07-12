53505817c73fa8b7612516126aa599a46d355bbe
ouyangsiying <785339684@qq.com>
2018-07-12 15:08:59 +0800

    学习爬虫

--- study_spider/spider_zhihu.py
+++ study_spider/spider_zhihu.py
@@ -6,7 +6,7 @@ import time
 
 class spider:
     def __init__(self):
-        self.url = r"https://www.zhihu.com/question/22918070"
+        self.url = r"http://image.baidu.com/search/index?tn=baiduimage&ct=201326592&lm=-1&cl=2&ie=gb18030&word=%B7%E7%BE%B0&fr=ala&ala=1&alatpl=adress&pos=0&hs=2&xthttps=000000"
         self.header = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'}
 
     def spider_zhihu(self):
@@ -14,14 +14,13 @@ class spider:
         page_info = request.urlopen(page).read().decode("utf-8")
         soup = BeautifulSoup(page_info, 'html.parser')
 
-        links = soup.find_all('img', 'origin_image zh-lightbox-thumb lazy',src = re.compile(r'.jpg'))
-        local_path = r'E:\pic'
-        print(0)
+        # links = soup.find_all('img', 'origin_image zh-lightbox-thumb lazy',src = re.compile(r'.jpg'))
+        links = soup.find_all('img','main_img img-hover')
         print(links)
+        local_path = r'E:\\python\\study\\study_spider\\pic\\'
         for link in links:
-            print(1)
-            print(link.attrs["src"])
-            request.urlretrieve(link.attrs["src"],local_path + r'%s.jpg' %time.time())
+            print(link.attrs["data-imgurl"])
+            request.urlretrieve(link.attrs["data-imgurl"],local_path + r'%s.jpg' %time.time())
 
 
 spider().spider_zhihu()
